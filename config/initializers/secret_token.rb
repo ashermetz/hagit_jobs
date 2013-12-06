cd0fa967d292b8a07123935a9d98c72a36225443
ashermetz <ashermetzger@hotmail.com>
2013-12-06 14:17:20 +0200

    creation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HagitJobs::Application.config.secret_token = 'e27b35df0d0e3c32c05dcb06fa0e855837b6e5a663357ffd0ada6f353295d27dc8a82fe3195aaceac537c7678cf8a8947cbdc892ca86158c250bf0d00e6c3adb'
