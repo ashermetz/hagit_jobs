488104d893224da4acb74b69a89844dde5a71e2e
ashermetz <ashermetzger@hotmail.com>
2013-12-06 20:53:33 +0200

    links and all

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,4 +1,6 @@
 class Job < ActiveRecord::Base
   attr_accessible :add_hour_per, :description, :job, :pay_per_hour
-    validates :job, :presence=> true
+    validates :job, :presence=> true, :uniqueness=> true
+    
+    has_many :hours
 end
